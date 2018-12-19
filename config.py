4c848da27a0ed1ebba632ef66bfdbc12cc98f195
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-12-19 15:57:17 +0100

    added create method for config

--- config.py
+++ config.py
@@ -9,10 +9,15 @@ class Config:
 
     def __init__(self, config_file):
         self.config_file = Path(config_file)
-        self.parse_config_file()
 
     def parse_config_file(self):
         with self.config_file.open(encoding='utf-8') as config_file:
             config = json.load(config_file)
 
         self.autthoken = config["AUTHTOKEN"]
+
+    @staticmethod
+    def create_config(config_file):
+        config = Config(config_file)
+        config.parse_config_file()
+        return config
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from config import Config
 
 if __name__ == '__main__':
     config_file = 'config.json'
-    config = Config(config_file)
+    config = Config.create_config(config_file)
 
     dax = Dax(config.autthoken)
     dax.get_ticker_data()
